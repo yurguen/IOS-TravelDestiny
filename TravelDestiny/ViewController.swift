de7d2fe66532ec94ed3e528f38df3431483d4f8f
internet <internet@internets-Mac-3.local>
2015-07-03 11:07:29 -0700

    Initial Commit

---
+++ TravelDestiny/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TravelDestiny
+//
+//  Created by internet on 7/3/15.
+//  Copyright (c) 2015 pinedo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
